a2b28acbcddc61154a1b422b346f9f548ba46939
boon4681 <46645814+boon4681@users.noreply.github.com>
2023-03-21 21:38:56 +0700

    update

--- pages/flower-docs.tsx
+++ pages/flower-docs.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next"
+import Head from "next/head"
 import { useEffect, useState } from "react"
 import Code from "../components/Code"
 import Header from "../components/Header"
@@ -6,9 +7,28 @@ import Header from "../components/Header"
 const FlowerDocs: NextPage = () => {
     const [all, setAll] = useState<any>({})
     useEffect(() => {
-        fetch('https://raw.githubusercontent.com/boon4681/itemsflower/1.19.2/all.json').then((res) => res.json()).then((data) => {setAll(data)})
+        fetch('https://raw.githubusercontent.com/boon4681/itemsflower/1.19.2/all.json').then((res) => res.json()).then((data) => { setAll(data) })
     }, [])
     return <>
+        <Head>
+            <meta charSet="UTF-8" />
+            <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Itemsflower</title>
+            <meta name="theme-color" content="#FFFFFF" />
+            <meta name="title" content="items" />
+            <meta name="description" content="Itemsflower provided colors for template based blocks and items." />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://items.boon4681.com/" />
+            <meta property="og:title" content="items" />
+            <meta property="og:description" content="Itemsflower provided colors for template based blocks and items." />
+            <meta property="og:image" content="https://items.boon4681.com/items.svg" />
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://items.boon4681.com/" />
+            <meta property="twitter:title" content="items" />
+            <meta property="twitter:description" content="Itemsflower provided colors for template based blocks and items." />
+            <meta property="twitter:image" content="https://items.boon4681.com/items.svg" />
+        </Head>
         <div className='min-h-screen'>
             <Header></Header>
             <div className='flex items-center flex-col w-full min-h-full'>
